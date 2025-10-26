35dec9f40e4a095f78cb7fe8e44fa53b746f2663
DuyDal <2251010015dal@ou.edu.vn>
2025-10-26 18:07:14 +0700

    Thay doi 26/10

--- karaokeapp/app/__init__.py
+++ karaokeapp/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, template_folder='templates')  # 👈 Khai báo đúng vị trí templates
+app = Flask(__name__, template_folder='templates')  #Khai báo đúng vị trí templates
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:123456789@localhost/karaokedb?charset=utf8mb4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "karaoke_secret_key"
